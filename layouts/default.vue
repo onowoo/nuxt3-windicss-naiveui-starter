d38a3bac177cea3302da2036f875c6c2f7f0363a
onowoo <6559170@qq.com>
2023-02-22 08:17:22 +0800

    layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,49 +1,16 @@
 <template>
-  <main class="py-20 px-10 text-center">
-    <div class="h-40 w-40 dark:bg-dark-500 dark:text-light text-dark-50">
-      这里是测试数据
+<div class="flex flex-col justify-between min-h-screen min-w-screen">
+  <header class="h-15 shadow-md">
+    <div class="max-w-300 h-[100%] mx-auto flex justify-between items-center">
+      <div>logo</div>
+      <div class="flex items-center gap-8">
+        <div v-for="i in 6" :key="i">menu</div>
+        <UiTheme />
+      </div>
+
     </div>
-    <div class="mt-5 mx-auto text-center dark:bg-dark-600 text-light-100 text-sm">
-      <UiTheme />
-      <n-space>
-    <n-button>Default</n-button>
-    <n-button type="tertiary">
-      Tertiary
-    </n-button>
-    <n-button type="primary">
-      Primary
-    </n-button>
-    <n-button type="info">
-      Info
-    </n-button>
-    <n-button type="success">
-      Success
-    </n-button>
-    <n-button type="warning">
-      Warning
-    </n-button>
-    <n-button type="error">
-      Error
-    </n-button>
-  </n-space>
-      <NCard
-        title="卡片分段示例"
-        :segmented="{
-          content: true,
-          footer: 'soft',
-        }"
-      >
-        <template #header-extra>
-          #header-extra
-        </template>
-        卡片内容
-        <template #footer>
-          #footer
-        </template>
-        <template #action>
-          #action
-        </template>
-      </NCard>
-    </div>
-  </main>
+  </header>
+  <main class="flex-auto"><slot /></main>
+  <footer class="h-15 border-t flex justify-center items-center">footer</footer>
+</div>
 </template>
