fd46926e9ea5132cfac15ddaa53da35064c0cccb
onowoo <6559170@qq.com>
2023-04-10 16:16:28 +0800

    11

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
-  <UiSwiper :bannerList="swiperImg"/>
+  <UiSwiper />
   <div class="max-w-[1200px] mx-auto">
     <UiGird />
   </div>
   <UiPagination />
 </template>
 <script setup>
-const { data } = await getConfig()
-const swiperImg = data.value.bannerList.map((obj) => {
-    return { ...obj }; 
-})
-// console.log(swiperImg);
+const { data:post } = await getArchives()
+
+const bannerPosts = JSON.parse(JSON.stringify(post.value.pageList.data))
+
+console.log(JSON.parse(JSON.stringify(post.value.pageList.data)));
 </script>
